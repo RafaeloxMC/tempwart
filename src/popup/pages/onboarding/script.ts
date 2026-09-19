25cb193cd78930df85652890ee17cd9753199085
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-09-19 14:04:44 +0200

    Save temporary login information in localStorage

--- src/popup/pages/onboarding/script.ts
+++ src/popup/pages/onboarding/script.ts
@@ -22,7 +22,41 @@ async function finalizeInputs() {
 	window.location.href = "/popup/pages/onboarding/account/index.html";
 }
 
+function fillInputs() {
+	const serverUrlInput = document.getElementById(
+		"server_url",
+	) as HTMLInputElement | null;
+	const apiKeyInput = document.getElementById(
+		"api_key",
+	) as HTMLInputElement | null;
+
+	const tempURL = localStorage.getItem("temp_url");
+	const tempKey = localStorage.getItem("temp_key");
+
+	if (serverUrlInput) serverUrlInput.value = tempURL ?? "";
+	if (apiKeyInput) apiKeyInput.value = tempKey ?? "";
+}
+
+async function onUrlChange() {
+	const serverUrlInput = document.getElementById(
+		"server_url",
+	) as HTMLInputElement | null;
+	localStorage.setItem("temp_url", serverUrlInput?.value ?? "");
+}
+
+async function onKeyChange() {
+	const apiKeyInput = document.getElementById(
+		"api_key",
+	) as HTMLInputElement | null;
+	localStorage.setItem("temp_key", apiKeyInput?.value ?? "");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	const continueBtn = document.getElementById("continue_btn");
+	const serverUrlInput = document.getElementById("server_url");
+	const apiKeyInput = document.getElementById("api_key");
+	fillInputs();
 	continueBtn?.addEventListener("click", async () => await finalizeInputs());
+	serverUrlInput?.addEventListener("change", async () => await onUrlChange());
+	apiKeyInput?.addEventListener("change", async () => await onKeyChange());
 });
